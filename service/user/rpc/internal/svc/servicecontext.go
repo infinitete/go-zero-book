aba94e76de3d30115c8431bf18637246626f0b9c
renshan <renshan@2155.io>
2023-03-27 22:31:48 +0800

    完成rpc

--- service/user/rpc/internal/svc/servicecontext.go
+++ service/user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "book/service/user/rpc/internal/config"
+import (
+	"book/service/user/model"
+	"book/service/user/rpc/internal/config"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
 	}
 }
